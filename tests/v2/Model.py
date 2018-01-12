918f33b042f91ab0e6bf1bc87c2827ab2fe3efe9
Aljosha Friemann <a.friemann@automate.wtf>
2018-01-12 17:11:30 +0100

    added test for oldstyle mutability problem and fixed py2 tests

--- tests/v2/Model.py
+++ tests/v2/Model.py
@@ -1,14 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-.. module: TODO
-    :platform: TODO
-    :synopsis: TODO
-
-.. moduleauthor:: Aljosha Friemann a.friemann@automate.wtf
-"""
-
-import unittest
-
 
 from simple_model.v2 import Model, Attribute
 
@@ -17,75 +7,89 @@ from simple_model.v2 import Model, Attribute
 @Attribute('foo', type=str, help='foo is a str')
 @Attribute('bar', type=int, optional=True, mutable=False)
 @Attribute('baz', type=int, default=12)
-class TestModel:
+class TestModel(object):
     pass
 
 
-class ModelV2Test(unittest.TestCase):
-    def test_creation(self):
-        m = TestModel(foo='abc')
+def test_creation():
+    m = TestModel(foo='abc')
 
-        self.assertIsNotNone(m)
+    assert m is not None
 
-        self.assertEqual(m.foo, 'abc')
-        self.assertEqual(m.baz, 12)
-        self.assertIsNone(m.bar)
+    assert m.foo == 'abc'
+    assert m.baz == 12
+    assert m.bar is None
 
-        self.assertEqual(m['foo'], 'abc')
+    assert m['foo'] == 'abc'
 
-        with self.assertRaises(KeyError):
-            m['fooo']
+    try:
+        m['fooo']
+        assert False, 'did not raise KeyError for unknown attribute'
+    except KeyError:
+        pass
 
-    def test_mutability(self):
-        m = TestModel(foo='abc')
 
-        m.foo = 'fofo'
+def test_mutability():
+    m = TestModel(foo='abc')
 
-        self.assertEqual(m.foo, 'fofo')
+    assert m.foo == 'abc'
 
-        with self.assertRaises(AttributeError):
-            m.bar = 'abc'
+    print('mutating foo')
+    m.foo = 'fofo'
 
-    def test_attribute_aliasing(self):
-        @Model()
-        @Attribute('foobar', type=str, alias='@foobar')
-        class AliasModel:
-            pass
+    assert m.foo == 'fofo'
 
-        m = AliasModel(**{'@foobar': 'abc'})
+    try:
+        m.bar = 'abc'
+        assert False, 'Attribute is mutable'
+    except AttributeError:
+        pass
 
-        self.assertEqual(m.foobar, 'abc')
-        self.assertEqual(m['@foobar'], 'abc')
 
-        del m
+def test_attribute_aliasing():
+    @Model()
+    @Attribute('foobar', type=str, alias='@foobar')
+    class AliasModel:
+        pass
 
-        m = AliasModel(foobar='abc')
+    m = AliasModel(**{'@foobar': 'abc'})
 
-        self.assertEqual(m.foobar, 'abc')
-        self.assertEqual(m['@foobar'], 'abc')
+    assert m.foobar == 'abc'
+    assert m['@foobar'] == 'abc'
+    assert m.foobar is m['@foobar']
 
-    def test_model_stacking(self):
-        @Model()
-        @Attribute('foobar', type=TestModel)
-        class StackedModel:
-            pass
+    del m
 
-        s = StackedModel(foobar={'foo': 'abc'})
+    m = AliasModel(foobar='abc')
 
-        self.assertIsNotNone(s)
-        self.assertIsInstance(s.foobar, TestModel)
+    assert m.foobar == 'abc'
+    assert m['@foobar'] == 'abc'
+    assert m.foobar is m['@foobar']
 
-        del s
 
-        m = TestModel(foo='abc')
+def test_model_stacking():
+    @Model()
+    @Attribute('foobar', type=TestModel)
+    class StackedModel:
+        pass
 
-        s = StackedModel(foobar=m)
-        self.assertIsNotNone(s)
-        self.assertIsInstance(s.foobar, TestModel)
+    s = StackedModel(foobar={'foo': 'abc'})
 
-        self.assertEqual(m, s.foobar)
+    assert s is not None
+    assert isinstance(s.foobar, TestModel)
 
-        self.assertEqual(StackedModel(**s), s)
+    del s
 
+    m = TestModel(foo='abc')
+
+    s = StackedModel(foobar=m)
+
+    assert s is not None
+    assert isinstance(s.foobar, TestModel)
+
+    assert m == s.foobar
+    assert m is not s.foobar
+
+    assert StackedModel(**s) == s
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
