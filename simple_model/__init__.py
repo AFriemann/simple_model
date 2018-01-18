6bb10c1854ca4dc4ebf887e5783e1d470c507f9e
Aljosha Friemann <a.friemann@automate.wtf>
2018-01-18 19:03:10 +0100

    set version to reasonable increase

--- simple_model/__init__.py
+++ simple_model/__init__.py
@@ -7,7 +7,7 @@
 .. moduleauthor:: Aljosha Friemann a.friemann@automate.wtf
 """
 
-__version__ = '2.0.0'
+__version__ = '1.2.0'
 
 from simple_model.helpers import list_type, one_of
 from simple_model.v1 import Model, Attribute
