2267a03d86a9178d6db43c634442faf27695235c
Aljosha Friemann <a.friemann@automate.wtf>
2018-01-27 17:08:09 +0100

    modified version without memory issues

--- simple_model/v2/__init__.py
+++ simple_model/v2/__init__.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-.. module:: simple_model.v2
-   :platform: Unix
-   :synopsis: Simple decorator based models for easy data (de-)serialization and validation.
-
-.. moduleauthor:: Aljosha Friemann a.friemann@automate.wtf
-
-"""
 
 import copy
 
@@ -14,20 +6,7 @@ import copy
 Unset = Ellipsis
 
 
-class ModelError(RuntimeError):
-    def __str__(self):
-        def format_arg(arg):
-            return '- attribute: {}\n  value: "{}"\n  exception: {}'.format(*arg).strip()
-
-        return '{name}\n{errors}'.format(
-            name=self.args[0],
-            errors='\n'.join(
-                format_arg(arg) for arg in self.args[1]
-            )
-        )
-
-
-class Model:
+class Model(object):
     def __init__(self, mutable=False, hide_unset=False, drop_unknown=False, ignore_unknown=True):
         self.mutable = mutable
         self.hide_unset = hide_unset
@@ -35,80 +14,57 @@ class Model:
         self.ignore_unknown = ignore_unknown
 
     def __call__(self, model):
-        custom_init = getattr(model, '__init__', None)
+        old_init = getattr(model, '__init__', None)
 
-        def __init__(cls, *args, **kwargs):
-            # make this instance memory independent from it's class.
-            cls.__attributes__ = copy.deepcopy(cls.__attributes__)
-
-            errors = []
-
+        def new_init(cls, *args, **kwargs):
             for attribute in cls.__attributes__:
-                if attribute.is_set():
-                    # skip attributes that have already been set.
-                    continue
+                if hasattr(cls, attribute.name):
+                    if getattr(cls, attribute.name) is not Unset:
+                        continue
 
-                value = kwargs.pop(attribute.name, kwargs.pop(
-                                   attribute.alias,
-                                   Unset))
+                value = kwargs.pop(attribute.name,
+                                   kwargs.pop(attribute.alias, Unset))
 
                 try:
-                    attribute.set(value=value)
-                except (AttributeError, ValueError) as e:
-                    errors.append((attribute, value, e))
+                    attribute.fset(cls, value)
+                except ValueError as e:
+                    exception = ValueError(attribute.name, value, e)
+                    exception.__cause__ = None
+                    raise exception
 
-                mutable = attribute.mutable or (self.mutable and attribute.mutable is None)
+            if old_init:
+                old_init(cls, *args, **kwargs)
 
-                prop = property(
-                    fget=attribute.get,
-                    fset=attribute.set if mutable else None,
-                    fdel=attribute.unset if mutable else None,
-                    doc=attribute.help
-                )
+        if old_init:
+            new_init.__doc__ = old_init.__doc__
 
-                setattr(model, attribute.name, prop)
-
-            if kwargs:
-                if self.drop_unknown:
-                    kwargs = {}
-                elif not self.ignore_unknown:
-                    errors.extend(
-                        (None, v, AttributeError('Unknown attribute "%s"' % k)) for k, v in kwargs.items()
-                    )
-
-            if errors:
-                raise ModelError(cls.__class__.__name__, errors)
-
-            if custom_init:
-                custom_init(cls, *args, **kwargs)
-
-        if custom_init is not None:
-            __init__.__doc__ = custom_init.__doc__
-
-        def __getitem__(cls, key):
-            for attribute in cls.__attributes__:
-                if attribute.name == key or attribute.alias == key:
+        def getitem(cls, key):
+            for a in cls.__attributes__:
+                if a.name == key or a.alias == key:
                     try:
-                        return dict(attribute.get())
+                        return dict(getattr(cls, a.name))
                     except (ValueError, TypeError):
-                        return attribute.get()
+                        return getattr(cls, a.name)
 
             raise KeyError(key)
 
-        model.__init__ = __init__
-        model.__getitem__ = __getitem__
+        model.__init__ = new_init
+        model.__getitem__ = getitem
+
         model.__str__ = lambda cls: str(dict(cls))
         model.__repr__ = lambda cls: str(dict(cls))
         model.__ne__ = lambda cls, o: not cls.__eq__(o)
-        model.__eq__ = lambda cls, o: (isinstance(o, cls.__class__) and dict(cls) == dict(o))
-        model.__contains__ = lambda cls, key: next((a for a in cls.__attributes__ if a.name == key and (a.is_set() or not self.hide_unset)), Unset) is not Unset
-        model.keys = lambda cls: sorted([ a.alias or a.name for a in cls.__attributes__ if (a.is_set() or not self.hide_unset)])
+        model.__eq__ = lambda cls, o: (issubclass(o.__class__, cls.__class__) and dict(cls) == dict(o))
+        model.__contains__ = lambda cls, key: getattr(cls, key) not in [None, Unset]
+        model.keys = lambda cls: sorted(
+            [a.name for a in cls.__attributes__ if (getattr(cls, a.name) not in [None, Unset] or not self.hide_unset)]
+        )
 
         return model
 
 
-class Attribute:
-    def __init__(self, name, type, optional=False, nullable=False, default=None, fdefault=None, mutable=None, alias=None, help=None):
+class Attribute(object):
+    def __init__(self, name, type, optional=False, nullable=False, mutable=True, default=None, fdefault=None, alias=None, help=None, value_by_reference=False):
         self.name = name
         self.type = type
         self.default = default
@@ -118,14 +74,10 @@ class Attribute:
         self.mutable = mutable
         self.alias = alias
         self.help = help
+        self.value_by_reference = value_by_reference
 
-        try:
-            if self.default is not None:
-                self.parse(self.default)
-            elif self.fdefault is not None:
-                self.parse(self.fdefault())
-        except Exception as e:
-            raise ValueError("Invalid default value(s) (%s/%s) for type %s" % (self.default, self.fdefault, self.type), e)
+    def __repr__(self):
+        return str(vars(self))
 
     def __call__(self, model):
         if not hasattr(model, '__attributes__'):
@@ -138,48 +90,52 @@ class Attribute:
         finally:
             model.__attributes__.add(self)
 
+        setattr(model, self.value_name, Unset)
+
+        prop = property(
+            fget=self.fget,
+            fset=self.fset if self.mutable else None,
+            fdel=self.fdel if self.mutable else None,
+            doc=self.help
+        )
+
+        setattr(model, self.name, prop)
+
         return model
 
-    def __repr__(self):
-        return str(vars(self))
+    def fget(self, cls):
+        return getattr(cls, self.value_name)
 
-    def set(self, model=None, value=Unset):
+    def fset(self, cls, value):
         if value is Unset:
-            try:
-                value = self.get_default()
-            except AttributeError as e:
-                pass
+            if self.default is not None:
+                value = self.default
+            elif self.fdefault is not None:
+                value = self.fdefault()
 
-        try:
-            self.value = self.parse(value)
-        except NotImplementedError as e:
-            raise e
-        except Exception as e:
-            raise ValueError("Invalid value for Attribute: %s" % value, e)
+        setattr(cls, self.value_name, self.parse(value))
 
-    def get(self, model=None):
-        try:
-            return self.value
-        except AttributeError as e:
-            if not self.optional:
-                raise e
+    def fdel(self, cls):
+        setattr(cls, self.value_name, Unset)
 
-    def unset(self, model=None):
-        del self.value
+    @property
+    def value_name(self):
+        return '_%s' % self.name
 
     def parse(self, value):
         if value is Unset:
             if self.optional:
                 return value
             else:
-                raise ValueError("Non-optional attribute set with non-value.")
+                raise ValueError("Attribute is not optional")
         elif value is None:
             if self.nullable:
                 return None
             else:
                 value = self.get_default()
 
-        value = copy.deepcopy(value)
+        if not self.value_by_reference:
+            value = copy.deepcopy(value)
 
         if self.type is None:
             return value
@@ -188,19 +144,5 @@ class Attribute:
             return self.type(**value)
         except TypeError:
             return self.type(value)
-
-    def is_set(self):
-        try:
-            return self.value is not Unset
-        except AttributeError:
-            return False
-
-    def get_default(self):
-        if self.default is not None:
-            return self.default
-        elif self.fdefault is not None:
-            return self.fdefault()
-        else:
-            raise AttributeError("Attribute has no default")
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
--- tests/v2/Problems.py
+++ tests/v2/Problems.py
@@ -13,17 +13,5 @@ class TestModel:
     pass
 
 
-def test_immutability_fails_with_oldstyle_class():
-    m = TestModel(foo='abc')
-
-    if sys.version_info.major > 2:
-        try:
-            m.foo = 'def'
-            assert False, 'immutable attribute is mutable'
-        except AttributeError:
-            pass
-    else:
-        m.foo = 'def'
-
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
